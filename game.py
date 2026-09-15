e8969bf7575f80cac236ee074e09067015bd9889
immanuelrobinson-commits <immanuel.robinson@ascte.org>
2026-09-15 08:55:34 -0500

    added hero and added fight to the end

--- game.py
+++ game.py
@@ -1,7 +1,23 @@
 from goblin import Goblin
+import hero
 
 
 ARENA_NAME = "The Iron Square"
+
+def battle(hero: hero.Hero, enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+
+        if hero.is_alive():
+            print(f"{hero.name} has defeated {enemy.name}!")
+        else:
+            print(f"{enemy.name} has defeated {hero.name}!")
+
+
 
 
 def main():
@@ -18,5 +34,14 @@ def main():
     print(f"{goblinTwo.name} enters the arena with {goblin.health} health.")
 
     print("But no hero has answered the call... yet.")
+
+
+
+    greg = hero.Hero("greg")
+    print(f"{greg.name} enters the arena with {greg.health} health.")
+    herodamage = greg.attack()
+    goblin.take_damage(herodamage)
+    battle(greg, goblin)
+
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,23 @@
+import random
 class Hero:
-    """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.health = 120
+        self.attack_power = 20
+
+    def attack(self):
+        return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+         if self.health < 0:
+            self.health = 0
+         print(f"{self.name} takes {damage} damage. Health: {self.health}")
+
+    def is_alive(self):
+        return self.health > 0
+
+
+
+
+           
